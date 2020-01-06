5e314c7fe3340d9558d88f6a7d709311e2366ce7
rita02052 <45421882+rita02052@users.noreply.github.com>
2020-01-07 02:09:43 +0800

    Update Order_Process.sol

--- Order_Process.sol
+++ Order_Process.sol
@@ -1,1 +1,25 @@
+pragma solidity ^0.5.12;
 
+contract Order_Process{
+    address payable owner; 
+    string deliver_address;
+    address farmer;
+    address deliveryman;
+    uint price;
+    uint id;
+    
+    constructor(address  _farmer, uint _price, uint _id, string memory _deliver_address) public {
+        owner=msg.sender;
+        deliver_address = _deliver_address;
+        farmer = _farmer;
+        price = _price;
+        id = _id;
+    }
+    
+    //完成訂單
+     function Getdeliveryman() public returns(address){
+        return deliveryman;
+    }
+     function Setdeliveryman(address _deliveryman) public{
+       deliveryman  = _deliveryman;
+    }
