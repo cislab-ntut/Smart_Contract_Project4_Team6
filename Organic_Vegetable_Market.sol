3771bcd9bf8fb0c787b1fce284f11d9ea6028334
wendywuuu860207 <51086110+wendywuuu860207@users.noreply.github.com>
2020-01-07 09:31:13 +0800

    Update Organic_Vegetable_Market.sol

--- Organic_Vegetable_Market.sol
+++ Organic_Vegetable_Market.sol
@@ -4,27 +4,43 @@ import "./Test_Process.sol";
 import "./Order_Process.sol";
 
 contract Organic_Vegetable_Market{
+    address theSystem;
+    uint OrderID;
+    Test_Process tp;
+	//紀錄所有user的address
+    address[] all_user; 
+    struct Person {
+        uint job_type;
+		Order_Process[] ops;
+		uint balance;
+    }
+    //people陣列中記錄系統中所有用戶身分
+    mapping(address => Person) people;
+
+    constructor(uint8 ptype) public {
+        people[msg.sender].job_type = ptype;
+        theSystem = msg.sender;
+        OrderID = 0;
+    }
     
-	//確認已收到貨，完成訂單
-	function confirmOrder(uint _OrderID){
-	    Order_Process _theop = findOrder(_OrderID);
-	    pay_fee(theSystem,_theop.GetFarmer(),_theop.GetPrice()-0.5); //給農夫錢錢
-	    pay_fee(theSystem,_theop.Getdeliveryman(),0.5); //給運送員錢錢
-	    _theop.kill();
-	}
-	//尋找訂單
-	function findOrder(uint _OrderID) returns (Order_Process){
-	   for(unit i = 0; i < all_user.length; i++){
-			//若為消費者
-			if(people[allUser[i]].ptype == 4){
-			    for(uint j = 0; j < people[allUser[j]].length ; j++){
-			        //找出訂單
-			        if(people[allUser[i]].ops[j].GetID() == _OrderID){
-					    return people[allUser[i]].ops[j]; //回傳訂單
-				    }
-			    }
-			}
+	//除了系統以外的人註冊job_type
+	function register(uint8 ptype) public{
+	    people[msg.sender].job_type = ptype;
+	    //檢驗商跟物流業者付入場費；testers and logistics pay fee
+        if(ptype == 1 || ptype == 2){
+            pay_fee(msg.sender,theSystem, 1);
+			all_user.push(msg.sender);
+        }
+		//農夫入場建立檢驗流程
+        else if(ptype == 3){
+            people[msg.sender].balance = msg.sender.balance;
+			all_user.push(msg.sender);
+        }
+		else if(ptype == 4){
+            people[msg.sender].balance = msg.sender.balance;
+			all_user.push(msg.sender);
 		}
 	}
-	
-}
+	//支付費用
+    function pay_fee(address sender, address receiver, uint amount) public {
+        require(amount <= people[sender].balance, "Not enough funds.");
